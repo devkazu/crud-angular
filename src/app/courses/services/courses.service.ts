d2351f3d71a30a9b18c4f46dbd7d561b1cab60d8
leokazuyukinagatani <lknlkn15@hotmail.com>
2023-09-17 20:45:47 -0300

    feat: loading in table

--- src/app/courses/services/courses.service.ts
+++ src/app/courses/services/courses.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 import { Course } from '../model/course';
+import { delay, first, tap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
@@ -12,6 +13,10 @@ export class CoursesService {
   private readonly API = '/assets/courses.json';
 
   list() {
-    return this.httpClient.get<Course[]>(this.API);
+    return this.httpClient.get<Course[]>(this.API).pipe(
+      first(),
+      delay(5000000),
+      tap((course) => console.log(course))
+    );
   }
 }
