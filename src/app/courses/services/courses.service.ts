3fb74ec58ffef61ec9407d5e10818096ccbf9cde
leokazuyukinagatani <lknlkn15@hotmail.com>
2023-09-17 21:12:00 -0300

    feat: icon and pipe category

--- src/app/courses/services/courses.service.ts
+++ src/app/courses/services/courses.service.ts
@@ -13,9 +13,8 @@ export class CoursesService {
   private readonly API = '/assets/courses.json';
 
   list() {
-    return this.httpClient.get<Course[]>(this.API + 'd').pipe(
+    return this.httpClient.get<Course[]>(this.API).pipe(
       first(),
-      delay(5000000),
       tap((course) => console.log(course))
     );
   }
